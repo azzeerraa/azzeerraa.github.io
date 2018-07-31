b7cc8dbbf5fe965a1bcb68e86eeabc83490e878d
AzzeerRaa <pavlich1971@mail.ru>
2018-08-01 01:00:51 +0300

    new

---
+++ index.php
@@ -0,0 +1,19 @@
+!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="style/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Hanalei+Fill" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+	
+<a style="font-family:Hanalei;" src=" http://dayte2.com/php-sessions-authorization">PHP</a> 
+
+
+</body>
+
+</html>
